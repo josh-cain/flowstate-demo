3c2b46ba234fefa6526610c1d337f3017249501e
Josh Cain <josh@semper.reformanda.io>
2019-04-19 15:28:14 -0500

    add support for sessions using express-session

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(session({
+  secret: 'boy I tell ya whut',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  req.session.visits = isNaN(req.session.visits) ? 1 : req.session.visits + 1;
+  console.log("visits this session: " + req.session.visits);
   res.render('index', { title: 'Express' });
 });
 
